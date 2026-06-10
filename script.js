6bcae8bfcd4dad38baf1ac59e17882483ab15eea
sky_ <samiya16azad@gmail.com>
2026-06-11 01:02:24 +0600

    commit3

--- script.js
+++ script.js
@@ -3,24 +3,21 @@ let boardWidth = 750;
 let boardHeight = 250;
 let context;
 
-
-
 let dinoWidth = 88;
 let dinoHeight = 94;
 let dinoX = 50;
 let dinoY = boardHeight - dinoHeight;
 let dinoImg;
 
-
-
 let dino = {
     x : dinoX,
     y : dinoY,
-    width: dinoWidth,
-    height: dinoHeight
+    width : dinoWidth,
+    height : dinoHeight
 };
 
 
+let cactusInterval;
 
 let cactusArray = [];
 
@@ -28,11 +25,14 @@ let cactus1Width = 34;
 let cactus2Width = 69;
 let cactus3Width = 102;
 
+
 let cactusHeight = 70;
 let cactusX = 700;
 let cactusY = boardHeight - cactusHeight;
 
-let cactus1Img, cactus2Img, cactus3Img;
+let cactus1Img;
+let cactus2Img;
+let cactus3Img;
 
 let velocityX = -8;
 let velocityY = 0;
@@ -41,56 +41,45 @@ let gravity = 0.4;
 let gameOver = false;
 let score = 0;
 
-
-
 let highScore = localStorage.getItem("highScore");
 highScore = highScore ? Number(highScore) : 0;
 
-
-
 let isDucking = false;
 
-
-window.onload = function () {
+window.onload = function() {
     board = document.getElementById("board");
     board.width = boardWidth;
     board.height = boardHeight;
 
     context = board.getContext("2d");
 
-    dinoImg = new Image();
+    dinoImg  = new Image();
     dinoImg.src = "./dino.png";
-
 
     cactus1Img = new Image();
     cactus1Img.src = "./cactus1.png";
 
-
     cactus2Img = new Image();
     cactus2Img.src = "./cactus2.png";
-
 
     cactus3Img = new Image();
     cactus3Img.src = "./cactus3.png";
 
-
     requestAnimationFrame(update);
     setInterval(placeCactus, 1000);
-
 
     document.addEventListener("keydown", moveDino);
     document.addEventListener("keyup", stopDuck);
 };
 
 
-
-function update()  {
+function update() {
     requestAnimationFrame(update);
 
     if (gameOver) {
         context.fillStyle = "red";
-        context.font = "30px courier";
-        context.fillText("GAME OVER", 280, 100);
+        context.font = "30px Courier";
+        context.fillText("GAME OVER", 250, 120);
         return;
     }
 
@@ -100,32 +89,65 @@ function update()  {
     dino.y = Math.min(dino.y + velocityY, dinoY);
 
     if (isDucking) {
-        context.drawImage(dinoImg, dino.x, dino.y + 40, dino.width, dino.height);
+        context.drawImage(
+            dinoImg,
+            dino.x,
+            dino.y + 40,
+            dino.width,
+            dino.height
+        );
     } else {
-        context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
+        context.drawImage(
+            dinoImg,
+            dino.x,
+            dino.y,
+            dino.width,
+            dino.height
+        );
     }
 
-    for (let i=0; i < cactusArray.length; i++) {
+    for (let i =  0; i < cactusArray.length; i++) {
         let cactus = cactusArray[i];
         cactus.x += velocityX;
+        context.drawImage(
+            cactus.img, 
+            cactus.x,
+            cactus.y,
+            cactus.width,
+            cactus.height
+        );
 
-        context.drawImage(cactus.img, cactus.x, cactus.y, cactus.width, cactus.height);
+        if (detectCollision(dino, cactus)){
+            console.log(
+                "Dino:",
+                dino.x,
+                dino.y,
+                dino.width,
+                dino.height
+            );
 
-        if (detectCollision(dino, cactus)) {
+            console.log(
+                "Cactus:",
+                cactus.x,
+                cactus.y,
+                cactus.width,
+                cactus.height
+            );
             gameOver = true;
         }
     }
 
-    score++ ;
+    score++;
+
     if (score > highScore) {
         highScore = score;
         localStorage.setItem("highScore", highScore);
     }
 
     context.fillStyle = "black";
-    context.font = "20px courier";
+    context.font = "20px Courier";
     context.fillText("Score: " + score, 5, 20);
-    context.fillText("Best: " + highScore, 5, 40);
+    context.fillText("Best: " + highScore, 5, 45);
 }
 
 
@@ -133,11 +155,15 @@ function update()  {
 function moveDino(e) {
     if (gameOver) return;
 
-    if ((e.code == "Space" || e.code == "ArrowUp") && dino.y == dinoY) {
+    if (
+        (e.code === "Space" || e.code === "ArrowUp") &&
+        Math.abs(dino.y - dinoY) < 1
+    ) {
+        console.log("Jump!", dino.y);
         velocityY = -10;
     }
 
-    if(e.code == "ArrowDown") {
+    if (e.code === "ArrowDown") {
         isDucking = true;
     }
 }
@@ -145,7 +171,7 @@ function moveDino(e) {
 
 
 function stopDuck(e) {
-    if (e.code == "ArrowDown") {
+    if (e.code === "ArrowDown") {
         isDucking = false;
     }
 }
@@ -165,15 +191,13 @@ function placeCactus() {
 
     let chance = Math.random();
 
-    if (chance > 0.90) {
+    if(chance > 0.90) {
         cactus.img = cactus3Img;
         cactus.width = cactus3Width;
-    }
-    else if (chance > 0.70) {
+    } else if (chance > 0.70) {
         cactus.img = cactus2Img;
         cactus.width = cactus2Width;
-    }
-    else if (chance > 0.50) {
+    } else if (chance > 0.50) {
         cactus.img = cactus1Img;
         cactus.width = cactus1Width;
     } else {
@@ -191,10 +215,10 @@ function placeCactus() {
 
 function detectCollision(a, b) {
     return (
-        a.x < b.x + b.width &&
-        a.x + a.width > b.x &&
-        a.y < b.y + b.height &&
-        a.y + a.height > b.y
+        a.x + 15 < b.x + b.width &&
+        a.x + a.width - 15 > b.x &&
+        a.y + 10 < b.y + b.height &&
+        a.y + a.height - 10 > b.y
     );
 }
 
@@ -204,6 +228,14 @@ function restartGame() {
     gameOver = false;
     score = 0;
     cactusArray = [];
+
+    dino.x = dinoX;
     dino.y = dinoY;
+
     velocityY = 0;
+    isDucking = false;
+
+    clearInterval(cactusInterval);
+
+    cactusInterval = setInterval(placeCactus, 1000);
 }
